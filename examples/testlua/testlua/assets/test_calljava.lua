7e5fe2a30f5d87e8c2f40ebc61bfce6d649c69ab
srplab <srplab.cn@hotmail.com>
2018-05-30 22:19:52 +0800

    remove all

--- examples/testlua/testlua/assets/test_calljava.lua
+++
@@ -1,7 +0,0 @@
-print(JavaClass)
-
-val = JavaClass("from lua")
-val:callback(1234.4564)
-val:callback("sdfsdfsdfsdf")
-val:SetLuaObject({"aaa","bbb"});
-print("===========end=========")
--- examples/testruby/testruby/assets/test_calljava.rb
+++
@@ -1,8 +0,0 @@
-puts $JavaClass
-
-val = $JavaClass.new("from ruby")
-puts(val)
-val.callback(1234.4564)
-val.callback("sdfsdfsdfsdf")
-val.SetRubyObject(File);
-puts("===========end=========")
